a07f0fb793c3cfd1649451a1e2a5eab3255c2a1d
Marat Kazbekov <maratvmk@mail.ru>
2014-03-09 22:31:39 +0500

    misc

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+=begin
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAIO56L2EHQVCYO4IQ',
+    :aws_secret_access_key  => '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'
+  }
+  config.fog_directory  = 'kolesa'
+end
+=end
