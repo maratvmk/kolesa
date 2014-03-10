1929aa235cd0da7c215b6d208b1a0db4cd3c0958
Marat Kazbekov <maratvmk@mail.ru>
2014-03-10 17:12:30 +0500

    misc

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-#role :app, %w{deploy@example.com}
-#role :web, %w{deploy@example.com}
-#role :db,  %w{deploy@example.com}
+#role :app, %w{maratvmk@mail.ru}
+#role :web, %w{maratvmk@mail.ru}
+#role :db,  %w{maratvmk@mail.ru}
 
 # Extended Server Syntax
 # ======================
